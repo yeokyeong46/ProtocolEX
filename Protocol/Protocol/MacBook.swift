9a42b4d3cb309fd892ca62abc254d4a6226b97ac
Jinwook Huh <gjwlsdnr0115@gmail.com>
2022-01-26 11:00:55 +0900

    feat: MacBook 구현

---
+++ Protocol/Protocol/MacBook.swift
@@ -0,0 +1,23 @@
+//
+//  MacBook.swift
+//  Protocol
+//
+//  Created by Jinwook Huh on 2022/01/26.
+//
+
+import Foundation
+
+struct MacBook {
+    var charger: Chargeable
+    
+    let mAhPerHour: Double
+    let currentBatteryCapacity: Double
+    let maxBatteryCapacity: Double
+    
+    func chargeBattery(charger: Chargeable) {
+        let converted = charger.convert(chargeablemAhPerHour: mAhPerHour)
+        let chargeableBatteryCapacity = maxBatteryCapacity - currentBatteryCapacity
+        let hour = (chargeableBatteryCapacity / converted).rounded(.up)
+        print(hour)
+    }
+}
