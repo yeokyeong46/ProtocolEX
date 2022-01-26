ccfea5d6688595b89e1c1af493839870ad6c6230
cheshire.kwon <cheshire.kwon@kakaocorp.com>
2022-01-26 11:18:00 +0900

    feat: charger

---
+++ Protocol/Protocol/Charger.swift
@@ -0,0 +1,15 @@
+struct Charger: Chargeable {
+    let chargemAhPerHour: Double
+
+    init(_ chagermAh: Double) {
+        self.chargemAhPerHour = chagermAh
+    }
+
+    func convert(chargeablemAhPerHour: Double) -> Double {
+        if chargeablemAhPerHour < self.chargemAhPerHour {
+            return self.chargemAhPerHour - chargeablemAhPerHour
+        } else {
+            return self.chargemAhPerHour
+        }
+    }
+}
